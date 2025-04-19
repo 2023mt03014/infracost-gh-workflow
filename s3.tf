6dd48775302f752ea727d5fb3b4abf7cb33b9ab0
2023mt03014 <2023mt03014@wilp.bits-pilani.ac.in>
2025-04-19 14:53:24 +0530

    adding instance

--- s3.tf
+++ s3.tf
@@ -36,7 +36,7 @@ data "aws_ami" "amazon_linux_2" {
     values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
-/*
+
 resource "aws_instance" "free_tier" {
   ami           = data.aws_ami.amazon_linux_2.id
   instance_type = "t2.micro"          # Free‑tier eligible
@@ -48,7 +48,18 @@ resource "aws_instance" "free_tier" {
     Name = "FreeTierInstance"
   }
 }
-*/
+
+resource "aws_instance" "free_tier_2" {
+  ami           = data.aws_ami.amazon_linux_2.id
+  instance_type = "t2.micro"          # Free‑tier eligible
+
+  # Optional: allow SSH (you’ll need to create this key pair first in AWS)
+  # key_name = var.ssh_key_name
+
+  tags = {
+    Name = "FreeTierInstance"
+  }
+}
 
 # Optional: pass in your SSH key name via a variable
 # variable "ssh_key_name" {
