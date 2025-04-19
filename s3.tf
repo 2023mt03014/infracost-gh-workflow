d2323cf37abb90b148fee7df1e403267813222cd
2023mt03014 <2023mt03014@wilp.bits-pilani.ac.in>
2025-04-19 13:37:27 +0530

    testing infracost githubaction

---
+++ s3.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-unique-bucket-name"  # must be globally unique
+  acl    = "private"
+
+  tags = {
+    Name        = "ExampleBucket"
+    Environment = "Dev"
+  }
+}
